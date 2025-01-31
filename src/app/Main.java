2e42394eb7ccd040441f2c73fb39de952bce7923
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-31 12:55:02 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,22 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static final double CONVERT_KG = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter amount of weight in kilograms: ");
+        double kgs = scan.nextDouble();
+        double pounds = convertKgsToPounds(kgs);
+        System.out.printf("%nResult is %.2f pounds", pounds);
+    }
+
+    private static double convertKgsToPounds(double kgs) {
+        return kgs * CONVERT_KG;
+    }
+}
